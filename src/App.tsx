5b95153c4e8226cec7dd2c38d1e2841cd57cc5f8
Edivan de Castro <edivandecastro@gmail.com>
2026-01-22 16:50:05 -0300

    Refactor App component to enhance canvas layout and styling. Update content area to utilize flexbox for improved alignment and responsiveness, and adjust border styling for better visual feedback.

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,20 @@ function App() {
         {isComponentsSidebarOpen && <Sidebar onClose={() => setIsComponentsSidebarOpen(false)} />}
 
         <main id="desktop" className={`flex-1 bg-gray-50 bg-grid-pattern pt-8 pr-8 pb-20 pl-8 overflow-auto transition-all duration-200 ml-12 ${isComponentsSidebarOpen ? 'ml-[21rem]' : ''}`}>
-          <div id="canvas" className="relative bg-white border-2 border-blue-500 rounded-lg p-8 min-h-[600px] w-full">
+          <div id="canvas" className="relative bg-white border-2 active:border-blue-500 p-8 min-h-[600px] w-full h-full">
             <div id="main-tag" className="absolute top-2 left-2">
               <span className="inline-block px-2 py-1 bg-blue-500 text-white text-xs font-semibold rounded-full uppercase">
                 Main
               </span>
             </div>
 
-            <div id="content-area" className="mt-8">
+            <div
+              id="content-area"
+              className="mt-8 flex items-center justify-center h-full w-full"
+            >
 
             </div>
+
           </div>
         </main>
       </div>
